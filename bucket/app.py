96a08867c69c76a3b415b595e5236355558b8cb9
Keenan Graham <ksgraham@stanford.edu>
2023-01-11 14:29:34 -0800

    Add bucket access policies

--- bucket/app.py
+++ bucket/app.py
@@ -4,6 +4,7 @@ from aws_cdk import Environment
 from bucket.config import config
 
 from bucket.bucket_storage import BucketStorage
+from bucket.bucket_access_policies import BucketAccessPolicies
 
 
 ENVIRONMENT = Environment(
@@ -13,9 +14,17 @@ ENVIRONMENT = Environment(
 
 app = App()
 
-BucketStorage(
+bucket_storage = BucketStorage(
     app,
     'BucketStorage',
+    env=ENVIRONMENT,
+    termination_protection=True,
+)
+
+bucket_access_polices = BucketAccessPolicies(
+    app,
+    'BucketAccessPolicies',
+    bucket_storage=bucket_storage,
     env=ENVIRONMENT,
     termination_protection=True,
 )
--- bucket/tests/test_snapshot.py
+++ bucket/tests/test_snapshot.py
@@ -13,16 +13,16 @@ ENVIRONMENT = Environment(
 )
 
 
-def test_match_with_snapshot(snapshot):
+def test_bucket_storage_match_with_snapshot(snapshot):
     from bucket.bucket_storage import BucketStorage
     app = App()
-    stack = BucketStorage(
+    bucket_storage = BucketStorage(
         app,
         'BucketStorage',
         env=ENVIRONMENT
     )
     template = Template.from_stack(
-        stack
+        bucket_storage
     )
     snapshot.assert_match(
         json.dumps(
@@ -31,4 +31,32 @@ def test_match_with_snapshot(snapshot):
             sort_keys=True
         ),
         'bucket_storage_template.json'
+    )
+
+
+def test_bucket_access_policies_match_with_snapshot(snapshot):
+    from bucket.bucket_storage import BucketStorage
+    from bucket.bucket_access_policies import BucketAccessPolicies
+    app = App()
+    bucket_storage = BucketStorage(
+        app,
+        'BucketStorage',
+        env=ENVIRONMENT
+    )
+    bucket_access_polices = BucketAccessPolicies(
+        app,
+        'BucketAccessPolicies',
+        bucket_storage=bucket_storage,
+        env=ENVIRONMENT
+    )
+    template = Template.from_stack(
+        bucket_access_polices
+    )
+    snapshot.assert_match(
+        json.dumps(
+            template.to_json(),
+            indent=4,
+            sort_keys=True
+        ),
+        'bucket_access_policies_template.json'
     )
