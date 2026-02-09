4ad82734e1a1c378cbb89c14f3bca6559028505c
ottojolanki <otaljo@gmail.com>
2026-02-09 10:01:41 -0800

    limit-to-env (#18)

--- doi/docker/run-doi.sh
+++ doi/docker/run-doi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 igvf_to_crossref --portal-key ${PORTAL_KEY} --portal-secret-key ${PORTAL_SECRET_KEY} --crossref-login ${CROSSREF_LOGIN} --crossref-password ${CROSSREF_PASSWORD} \
---crossref-server ${CROSSREF_SERVER} --igvf-server ${IGVF_SERVER} --limit 3000
+--crossref-server ${CROSSREF_SERVER} --igvf-server ${IGVF_SERVER} --limit ${LIMIT}
